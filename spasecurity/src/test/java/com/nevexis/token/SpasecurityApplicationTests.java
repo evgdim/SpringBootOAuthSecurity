1b6bbf0695b69806d6a325ee9e584e2a8b08fd80
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2017-08-26 10:56:25 +0300

    upgraded to spring boot 1.5.6

--- spasecurity/src/test/java/com/nevexis/token/SpasecurityApplicationTests.java
+++ spasecurity/src/test/java/com/nevexis/token/SpasecurityApplicationTests.java
@@ -2,12 +2,12 @@ package com.nevexis.token;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpasecurityApplication.class)
+@SpringBootTest(classes = SpasecurityApplication.class)
 @WebAppConfiguration
 public class SpasecurityApplicationTests {
 
